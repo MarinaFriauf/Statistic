31d62526bbd7b2c2a42e14ba7c8322208ff122ea
MarinaFriauf <marina506506506@gmail.com>
2022-04-26 19:04:26 +0700

    finish

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,60 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public int calculateSum(long[] sales) {
+        int sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public int calcAvg(long[] sales) {
+        return calculateSum(sales) / sales.length;
+
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int overAvg(long[] sales) {
+        int counter = 0;
+            for (long sale : sales) {
+            if (sale > calcAvg(sales)) {
+                counter++;
+            }
+                   }
+        return counter;
+    }
+
+    public int lessAvg(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < calcAvg(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,80 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+        void shouldCalculateSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+
+        int actual = service.calculateSum(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalkAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = service.calcAvg(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        int actual = service.minSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+
+        int actual = service.maxSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldOverAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.overAvg(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldLessAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.lessAvg(sales);
+
+        assertEquals(expected, actual);
+
+    }
+}
